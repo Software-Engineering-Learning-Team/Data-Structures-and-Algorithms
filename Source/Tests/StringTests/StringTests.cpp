125a62502cf6adb46d62bb29650befbbd8ac5329
Andrew-c0d3 <andrewcode.commits@gmail.com>
2021-10-14 20:50:10 +0300

    Add empty string unit test

--- Source/Tests/StringTests/StringTests.cpp
+++ Source/Tests/StringTests/StringTests.cpp
@@ -1,8 +1,15 @@
 #include <gtest/gtest.h>
 #include <String/String.hpp>
 
-TEST(StringTest, Constructor) {
+TEST(StringTest, Constructor_with_nullptr) {
   DataStructures::String string(nullptr);
+  auto actual_length = string.get_length();
+  std::size_t expected_length = 0;
+  ASSERT_EQ(actual_length, expected_length);
+}
+
+TEST(StringTest, Constructor_with_empty_string) {
+  DataStructures::String string("");
   auto actual_length = string.get_length();
   std::size_t expected_length = 0;
   ASSERT_EQ(actual_length, expected_length);
