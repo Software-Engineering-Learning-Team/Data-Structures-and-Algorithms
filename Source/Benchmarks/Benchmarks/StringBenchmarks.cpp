84b8b468d30296d094db8d6b9ea7333487c7462f
Andrew-c0d3 <andrewcode.commits@gmail.com>
2021-10-24 23:51:37 +0300

    Add Google test & Benchmark as CMake dependencies to download
    - Remove folders with libraries (Google test & Google benchmark)

--- Source/Benchmarks/Benchmarks/StringBenchmarks.cpp
+++ Source/Benchmarks/Benchmarks/StringBenchmarks.cpp
@@ -5,7 +5,7 @@ static void BM_StdStringCreation(benchmark::State& state)
 {
 	for (auto _: state)
 	{
-		std::string test_string("String");
+		std::string test_string("String for benchmark");
 	}
 
 }
@@ -17,25 +17,10 @@ static void BM_CustomStringCreation(benchmark::State& state)
 {
 	for (auto _: state)
 	{
-		DataStructures::String test_string("String");
+		DataStructures::String test_string("String for benchmark");
 	}
 }
 
 BENCHMARK(BM_CustomStringCreation);
-
-static void BM_StringCompare(benchmark::State& state)
-{
-	std::string s1(state.range(0), '-');
-	std::string s2(state.range(0), '-');
-	for (auto _: state)
-	{
-		benchmark::DoNotOptimize(s1.compare(s2));
-	}
-	state.SetComplexityN(state.range(0));
-}
-
-BENCHMARK(BM_StringCompare)
-->RangeMultiplier(2)->Range(1 << 10, 1 << 18)->Complexity(benchmark::oN);
-
 
 BENCHMARK_MAIN();
